398469a60737b5ebf5f771c11380aae8d335da4f
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-29 20:49:35 +0300

    feat: create user model

---
+++ components/user/model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const { emailPattern } = require('../../utils/patterns');
+
+mongoose.set('toObject', { useProjection: true });
+mongoose.set('toJSON', { useProjection: true });
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'email не указан'],
+    unique: [true, 'Пользователь с указанным email уже существует'],
+    validate: {
+      validator: (v) => emailPattern.test(v),
+      message: 'Email не валиден',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Пароль не указан'],
+    select: false,
+  },
+  name: {
+    type: String,
+    required: [true, 'Имя не указанo'],
+    minlength: [2, 'Длина имени не должна быть короче двух символов'],
+    maxlength: [30, 'Длина имени не должна превышать 30 символов'],
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
