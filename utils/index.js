2afc08eeb05a8fce94dbbb85100b89165c7ac574
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-29 20:50:05 +0300

    feat: create isUrl function

---
+++ utils/index.js
@@ -0,0 +1,12 @@
+const { urlPattern } = require('./patterns');
+
+/**
+ * Checks if a string is a URL
+ * @param {string} str
+ * @returns boolean
+ */
+const isUrl = (str) => urlPattern.test(str);
+
+module.exports = {
+  isUrl,
+};
