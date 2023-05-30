2dd74d71a79ea1ceefe83c50e1c3e2908f640625
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-30 23:32:11 +0300

    feat: install cookie-parser and use it in the app, create cookie options for the token

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
 
 const { PORT, BD_URL } = require('./configs');
 const limiter = require('./configs/limiter-config');
@@ -16,6 +17,7 @@ app.use(limiter);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use(router);
 app.use(handleError);
