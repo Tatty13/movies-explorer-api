57a45fda7f3f032d999b9a3eca351fb2d7fe253a
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-30 22:28:16 +0300

    feat: install bcryptjs, create functions to generate hash and check password

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,12 @@
 const isUrl = require('./is-url');
 const handleMongooseValidationError = require('./handle-mongoose-validation-error');
 const handleMongooseCastError = require('./handle-mongoose-cast-error');
+const { generateHash, checkPassword } = require('./hash');
 
 module.exports = {
   isUrl,
   handleMongooseValidationError,
   handleMongooseCastError,
+  generateHash,
+  checkPassword,
 };
