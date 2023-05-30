6153535d611beb8b41d1745d26d17f8fe4b68c15
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-30 20:18:33 +0300

    refactor: move the isUrl function to a separate file

--- utils/index.js
+++ utils/index.js
@@ -1,14 +1,6 @@
-const { urlPattern } = require('./patterns');
-
+const isUrl = require('./is-url');
 const handleMongooseValidationError = require('./handle-mongoose-validation-error');
 const handleMongooseCastError = require('./handle-mongoose-cast-error');
-
-/**
- * Checks if a string is a URL
- * @param {string} str
- * @returns boolean
- */
-const isUrl = (str) => urlPattern.test(str);
 
 module.exports = {
   isUrl,
