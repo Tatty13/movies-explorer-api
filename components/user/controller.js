faae763729a23402c8f583bbda85450608ebcff3
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-30 22:35:25 +0300

    feat: add functionality to create user on /signup route

--- components/user/controller.js
+++ components/user/controller.js
@@ -2,6 +2,8 @@ const mongooseError = require('mongoose').Error;
 
 const User = require('./model');
 const { NotFoundError, BadRequestError } = require('../../errors');
+const { generateHash } = require('../../utils');
+const { CREATED_201 } = require('../../utils/constants');
 
 async function getUser(req, res, next) {
   const { _id } = req.user;
@@ -38,7 +40,20 @@ async function updateUser(req, res, next) {
   }
 }
 
+async function createUser(req, res, next) {
+  const { email, password, name } = req.body;
+  try {
+    const hash = await generateHash(password);
+    const user = await User.create({ email, name, password: hash });
+
+    res.status(CREATED_201).send({ user });
+  } catch (err) {
+    next(err);
+  }
+}
+
 module.exports = {
   getUser,
   updateUser,
+  createUser,
 };
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const router = require('express').Router();
 const usersRouter = require('../components/user/router');
 const moviesRouter = require('../components/movie/router');
 const notFoundRouter = require('../components/not-found/router');
+const { createUser } = require('../components/user/controller');
+
+router.post('/signup', createUser);
 
 router.use('/users', usersRouter);
 router.use('/movies', moviesRouter);
