82d11ead05cbce92d254228ee3fd39b608fcf945
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-05-31 12:53:59 +0300

    feat: create auth middleware, use it to protect all routes except signin and signup

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,14 @@ const usersRouter = require('../components/user/router');
 const moviesRouter = require('../components/movie/router');
 const notFoundRouter = require('../components/not-found/router');
 const { createUser, signIn, signOut } = require('../components/user/controller');
+const auth = require('../middlewares.js/auth');
 
 router.post('/signup', createUser);
 router.post('/signin', signIn);
-router.post('/signout', signOut);
+router.post('/signout', auth, signOut);
 
-router.use('/users', usersRouter);
-router.use('/movies', moviesRouter);
-router.use(notFoundRouter);
+router.use('/users', auth, usersRouter);
+router.use('/movies', auth, moviesRouter);
+router.use(auth, notFoundRouter);
 
 module.exports = router;
